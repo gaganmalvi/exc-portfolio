45c11c481b2bbad6dcd5a941176bba43e82ee355
Gagan Malvi <malvigagan@gmail.com>
2022-01-29 20:20:43 +0530

    fix(portfolio): drop unused objects/includes.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { SocialIcon } from "react-social-icons";
 import Logo from "./logo";
 import { Link } from "react-router-dom";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 
 export default function Projects() {
     return (
